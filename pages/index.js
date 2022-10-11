acbdfeabd36b2e38b5090dd0786db332a311d91d
Edgar Ribeiro Neto <edgarribeironeto@Edgars-MacBook-Air.local>
2022-10-11 16:59:34 -0300

    add api resale

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import PreFetchLink from './components/PreFetchLink'
+import PreFetchLink from '../components/PreFetchLink'
 
 export default function Home() {
   return (
--- pages/infinitescrollssr/index.js
+++ pages/infinitescrollssr/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useInfiniteQuery } from "react-query";
-import ContentInfiniteScroll from "../components/ContentInfiniteScroll";
+import ContentInfiniteScroll from "../../components/ContentInfiniteScroll";
 
 function InfiniteScrollPageSSR({ propertyList }) {
   return (
