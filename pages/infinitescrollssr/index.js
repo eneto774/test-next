acbd1be1d0e84f9a525d1c168b912117102f3d32
Edgar Ribeiro Neto <edgarribeironeto@Edgars-MacBook-Air.local>
2022-10-13 15:03:15 -0300

    add head on nextjs

--- pages/infinitescrollssr/index.js
+++ pages/infinitescrollssr/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Head from "next/head";
 import Image from "next/image";
 import React, { useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -7,12 +8,19 @@ import ContentInfiniteScroll from "../../components/ContentInfiniteScroll";
 
 function InfiniteScrollPageSSR({ propertyList }) {
   return (
+    <>
+    <Head>
+      <title>Resale NextJS - Test</title>
+      <meta name="description" content="Listing with Infinite Scroll Based in Resale Api." />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <div>
       <h1>
-        Rick and Morty and Infinite Scroll - SSR
+        Resale and Infinite Scroll - SSR
       </h1>
       <ContentInfiniteScroll data={propertyList?.data} paginationInfo={propertyList?.pagination} apiData={propertyList?.apiData} />
     </div>
+    </>
   );
 }
 
